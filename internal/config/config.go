377d0a09fc50ebe4737f3c642b0daf2039481afe
SSSo11o <ibronovaziz5@gmail.com>
2025-04-04 15:36:19 +0500

    add dockerfile

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,20 +1,36 @@
 package config
 
 import (
+	"fmt"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 	"log"
+	"os"
+	"time"
 )
 
 var DB *gorm.DB
 
 func ConnectDB() {
-	dsn := "host=localhost user=postgres password=1112 dbname=postgres port=5432 sslmode=disable"
-	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
-	if err != nil {
-		log.Fatal("Ошибка подключения к базе данных:", err)
+	host := os.Getenv("DATABASE_HOST")
+	user := os.Getenv("DATABASE_USER")
+	password := os.Getenv("DATABASE_PASSWORD")
+	dbname := os.Getenv("DATABASE_NAME")
+	port := os.Getenv("DATABASE_PORT")
+
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
+		host, user, password, dbname, port)
+
+	var err error
+	for i := 0; i < 10; i++ {
+		DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+		if err == nil {
+			log.Println("База данных успешно подключена")
+			return
+		}
+		log.Printf("Ошибка подключения к базе данных: %v, попытка %d\n", err, i+1)
+		time.Sleep(5 * time.Second)
 	}
 
-	DB = db
-	log.Println("База данных успешно подключена")
+	log.Fatal("Не удалось подключиться к базе данных после нескольких попыток")
 }
